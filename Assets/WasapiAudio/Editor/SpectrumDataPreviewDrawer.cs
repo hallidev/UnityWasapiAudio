1b162be071473e9de33515846a8ad41b68eb7f80
hallidev <jhalliday@gmail.com>
2021-01-10 12:41:22 -0500

    Some safety checks in property drawer

--- Assets/WasapiAudio/Editor/SpectrumDataPreviewDrawer.cs
+++ Assets/WasapiAudio/Editor/SpectrumDataPreviewDrawer.cs
@@ -18,51 +18,63 @@ namespace Assets.WasapiAudio.Editor
         // Start is called before the first frame update
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            var preview = fieldInfo.GetValue(property.serializedObject.targetObject) as SpectrumData;
-
-            if (preview == null)
+            if (property.serializedObject == null || property.serializedObject.targetObject == null)
             {
                 return;
             }
 
-            // Draw black background
-            EditorGUI.DrawRect(position, Color.black);
+            var preview = fieldInfo.GetValue(property.serializedObject.targetObject) as SpectrumData;
 
-            var x = 0.0f;
-            var barWidth = position.width / preview.Values.Length;
-            var average = preview.Values.Average() * DrawerHeight;
-
-            for (var i = 0; i < preview.Values.Length; i++)
+            if (preview == null || preview.Values == null)
             {
-                // Draw spectrum bar
-                var barHeight = preview.Values[i] * DrawerHeight;
-
-                var spectrumBarColor = Color.green;
-
-                if (barHeight >= average)
-                {
-                    spectrumBarColor = Color.yellow;
-                }
-
-                var spectrumBar = new Rect(position.x + x,
-                    position.y + DrawerHeight - barHeight,
-                    barWidth,
-                    barHeight);
-
-                EditorGUI.DrawRect(spectrumBar, spectrumBarColor);
-
-                // Draw average line
-                var averageLine = new Rect(position.x,
-                    position.y + DrawerHeight - average,
-                    position.width,
-                    barWidth / 2);
-
-                EditorGUI.DrawRect(averageLine, Color.blue);
-
-                x += barWidth;
+                return;
             }
 
-            EditorGUI.PrefixLabel(position, label);
+            try
+            {
+                // Draw black background
+                EditorGUI.DrawRect(position, Color.black);
+
+                var x = 0.0f;
+                var barWidth = position.width / preview.Values.Length;
+                var average = preview.Values.Average() * DrawerHeight;
+
+                for (var i = 0; i < preview.Values.Length; i++)
+                {
+                    // Draw spectrum bar
+                    var barHeight = preview.Values[i] * DrawerHeight;
+
+                    var spectrumBarColor = Color.green;
+
+                    if (barHeight >= average)
+                    {
+                        spectrumBarColor = Color.yellow;
+                    }
+
+                    var spectrumBar = new Rect(position.x + x,
+                        position.y + DrawerHeight - barHeight,
+                        barWidth,
+                        barHeight);
+
+                    EditorGUI.DrawRect(spectrumBar, spectrumBarColor);
+
+                    // Draw average line
+                    var averageLine = new Rect(position.x,
+                        position.y + DrawerHeight - average,
+                        position.width,
+                        barWidth / 4);
+
+                    EditorGUI.DrawRect(averageLine, Color.blue);
+
+                    x += barWidth;
+                }
+
+                EditorGUI.PrefixLabel(position, label);
+            }
+            catch
+            {
+
+            }
         }
     }
 }
