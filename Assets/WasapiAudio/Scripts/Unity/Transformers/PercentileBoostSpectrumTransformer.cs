a00f6d1efa1797112f549bd773b62d5b77bb5d93
hallidev <jhalliday@gmail.com>
2021-01-13 07:47:08 -0500

    Changing average boost to percentile boost

---
+++ Assets/WasapiAudio/Scripts/Unity/Transformers/PercentileBoostSpectrumTransformer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.WasapiAudio.Scripts.Unity.Transformers
+{
+    [Serializable]
+    public class PercentileBoostSpectrumTransformer : SpectrumTransformer
+    {
+        [Range(0.0f, 1.0f)]
+        public float Percentile = 0.5f;
+        public float BelowPercentileMultiplier = 0.5f;
+        public float AbovePercentileMultiplier = 2.0f;
+
+        protected override void PerformTransform(float[] input, ref float[] output)
+        {
+            var percentile = GetPercentile(input, Percentile);
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] <= percentile)
+                {
+                    output[i] = input[i] * BelowPercentileMultiplier;
+                }
+                else
+                {
+                    output[i] = input[i] * AbovePercentileMultiplier;
+                }
+            }
+        }
+
+        public float GetPercentile(float[] sequence, float percentile)
+        {
+            Array.Sort(sequence);
+            
+            var length = sequence.Length;
+            var n = (length - 1) * percentile + 1;
+            
+            // Another method: double n = (N + 1) * excelPercentile;
+            if (n == 1.0f)
+            {
+                return sequence[0];
+            }
+
+            if (n == length)
+            {
+                return sequence[length - 1];
+            }
+            
+            int k = (int)n;
+            float d = n - k;
+            
+            return sequence[k - 1] + d * (sequence[k] - sequence[k - 1]);
+        }
+    }
+}
