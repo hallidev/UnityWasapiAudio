599ba89a6f8dd4f3a8c8e46fcea43ef590b6fc59
Jon Halliday <jhalliday@gmail.com>
2022-04-21 09:24:11 -0400

    Updating single band effect. Now requires being fed audio data

--- Assets/WasapiAudio/Scripts/Unity/AudioVisualizationSpectrumBandEffect.cs
+++ Assets/WasapiAudio/Scripts/Unity/AudioVisualizationSpectrumBandEffect.cs
@@ -1,12 +1,31 @@
-﻿namespace Assets.WasapiAudio.Scripts.Unity
+﻿using UnityEngine;
+
+namespace Assets.WasapiAudio.Scripts.Unity
 {
-    public abstract class AudioVisualizationSpectrumBandEffect : AudioVisualizationEffect
+    public abstract class AudioVisualizationSpectrumBandEffect : MonoBehaviour
     {
+        private float[] _spectrumData;
+
         // Inspector properties
         public int SpectrumBand;
 
+        public void SetSpectrumData(float[] spectrumData)
+        {
+            _spectrumData = spectrumData;
+        }
+
+        protected float[] GetSpectrumData()
+        {
+            return _spectrumData;
+        }
+
         protected float GetSpectrumBandData()
         {
+            if (_spectrumData == null)
+            {
+                return 0.0f;
+            }
+
             return GetSpectrumData()[SpectrumBand];
         }
     }
